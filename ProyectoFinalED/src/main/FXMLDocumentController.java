d450ab124781002b6cb33c006ff3ef31f630cb02
Gabriela Sosa <gabiruca2009@hotmail.com>
2021-01-21 20:48:41 -0500

    Cambios pequeños a la interfaz, métodos para controlar el treemap agg

--- ProyectoFinalED/src/main/FXMLDocumentController.java
+++ ProyectoFinalED/src/main/FXMLDocumentController.java
@@ -5,12 +5,18 @@
  */
 package main;
 
+import Directorio.Directorio;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -20,6 +26,10 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    LinkedList<Directorio> treeMap;
+    private TextField mRuta;
+    private Button visualizar;
+    private AnchorPane mostrar;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -27,6 +37,30 @@ public class FXMLDocumentController implements Initializable {
         label.setText("Hello World!");
     }
     
+     //Métodos necesarios para el manejo del treeMap
+    public void iteration(int numero,LinkedList<Directorio> treeMap) {
+        Iterator it = treeMap.iterator();
+        while (it.hasNext()) {
+            Directorio siguiente = (Directorio) it.next();
+            if (siguiente.getDirectorios().size() > 0) {
+                System.out.println(tmIdentation(numero)+siguiente.getNombre()+"("+siguiente.getTamaño()+")");
+                iteration(numero+2,siguiente.getDirectorios());
+            } else {
+                System.out.println(tmIdentation(numero)+siguiente.getNombre()+"("+siguiente.getTamaño()+")");
+            }
+        }
+    }
+    public String tmIdentation(int numero) {
+        String cad = String.valueOf(numero);
+        if (numero > 0) {
+            for (int i = 0; i < numero; i++) {
+                cad =cad+"-";
+            }
+            return cad;
+        } else {
+            return "";
+        }
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
